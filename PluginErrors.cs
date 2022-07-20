2407f42b39f73001093c6923ce18e5df5dab8fa2
strick-j <skazoo@gmail.com>
2022-07-20 08:58:45 -0400

    Updated error codes

--- PluginErrors.cs
+++ PluginErrors.cs
@@ -14,10 +14,20 @@
         public static readonly int MANDATORY_PARAMETER_MISSING = 8801;
         public static readonly int REGEX_TOKEN_ERROR = 8802;
         public static readonly int REGEX_UUID_ERROR = 8803;
+        public static readonly int RESPONSE_CONTENT_NULL = 8804;
+        public static readonly int RESPONSE_GENERIC_EXCEPTION = 8505;
+        public static readonly int RESPONSE_TIMEOUT_EXCEPTION = 8506;
 
-        public static readonly int RESPONSE_CONTENT_NULL = 8810;
-        public static readonly int RESPONSE_GENERIC_EXCEPTION = 8511;
-        public static readonly int RESPONSE_TIMEOUT_EXCEPTION = 8512;
+        // SUCCESS_ERROR --> Errors when 200 Success is returned but the action failed.
+        public static readonly int SUCCESS_DEFAULT_ERROR = 8810;
+        public static readonly int SUCCESS_USER_NOT_FOUND = 8811;
+        public static readonly int SUCCESS_USER_ID_NOT_FOUND = 8812;
+        public static readonly int SUCCESS_USER_NOT_AUTHORIZED = 8813;
+        public static readonly int SUCCESS_SERVICE_NOT_AUTHORIZED = 8814;
+        public static readonly int SUCCESS_INVALID_PASSWORD_COMPLEXITY = 8815;
+        public static readonly int SUCCESS_PASSWORD_SAME_AS_OLD = 8816;
+        public static readonly int SUCCESS_PASSWORD_LAST_N = 8817;
+        public static readonly int SUCCESS_JSON_EXCEPTION = 8818;
 
         // BAD_REQUEST_ERRORS --> Errors returned with 400 Bad Request Response.
         public static readonly int BAD_REQUEST_NULL_RESPONSE = 8820;
@@ -46,16 +56,7 @@
         public static readonly int HTTP_INNER_UNHANDLED = 8844;
         public static readonly int HTTP_SSL_TLS_EXCEPTION = 8845;
 
-        // SUCCESS_ERROR --> Errors when 200 Success is returned but the action failed.
-        public static readonly int SUCCESS_DEFAULT_ERROR = 8850;
-        public static readonly int SUCCESS_USER_NOT_FOUND = 8851;
-        public static readonly int SUCCESS_USER_ID_NOT_FOUND = 8852;
-        public static readonly int SUCCESS_USER_NOT_AUTHORIZED = 8853;
-        public static readonly int SUCCESS_SERVICE_NOT_AUTHORIZED = 8854;
-        public static readonly int SUCCESS_INVALID_PASSWORD_COMPLEXITY = 8855;
-        public static readonly int SUCCESS_PASSWORD_SAME_AS_OLD = 8856;
-        public static readonly int SUCCESS_PASSWORD_LAST_N = 8857;
-        public static readonly int SUCCESS_JSON_EXCEPTION = 8858;
+        
 
         public static readonly int DEFAULT_ERROR_NUMBER = -1;
     }
